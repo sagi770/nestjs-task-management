85985fa680c0dcebfb2b993ea49f9ee34cbfbf47
sagi770 <sagi.770@gmail.com>
2020-11-16 19:00:59 +0200

    secend

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Task } from './tasks.model';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private TasksService: TasksService) { }
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.TasksService.getAllTasks()
+    }
+
+    @Post()
+    createTask(
+        @Body('title') title: string,
+        @Body('description') description: string,
+    ) {
+        return this.TasksService.createTask(title, description)
+    }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import * as uuid from 'uuid/v1';
+import { Task, TaskStatus } from './tasks.model';
+
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks: Task[] = []
+
+    getAllTasks(): Task[] {
+        return this.tasks
+    }
+
+    createTask(title: string, description: string): Task {
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN
+        }
+
+        this.tasks.push(task);
+
+        return task;
+    }
+}
