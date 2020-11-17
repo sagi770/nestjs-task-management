d10dbcd0da615ff9fb7b80f820f41699d080b845
sagi770 <sagi.770@gmail.com>
2020-11-17 03:25:04 +0200

    create CRUD on  local task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { Task } from './tasks.model';
+import { Body, Controller, Get, Post, Param, Delete, Patch } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task, TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -11,11 +12,26 @@ export class TasksController {
         return this.TasksService.getAllTasks()
     }
 
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.TasksService.getTaskById(id);
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id') id: string): void {
+        this.TasksService.deleteTask(id);
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(
+        @Param('id') id: string,
+        @Body('status') status: TaskStatus,
+    ): Task {
+        return this.TasksService.updateTaskStatus(id, status);
+    }
+
     @Post()
-    createTask(
-        @Body('title') title: string,
-        @Body('description') description: string,
-    ) {
-        return this.TasksService.createTask(title, description)
+    createTask(@Body() createTaskDto: CreateTaskDto) {
+        return this.TasksService.createTask(createTaskDto)
     }
 }
